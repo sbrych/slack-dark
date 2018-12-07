ef3a5f4c2d09b70a6fcdb8395bd99ab5c4ad59f6
Sebastian Brych <sebastian+git@nwscx.com>
2018-12-07 13:57:12 +0100

    add github url

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Originally created by [a7madgamal](https://gist.github.com/a7madgamal/c2ce04dde8520f426005e5ed28da8608)
+Sidebar theme should be changed to a dark one.
--- snippet.js
+++ snippet.js
@@ -7,7 +7,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   let tt__customCss = `.menu ul li a:not(.inline_menu_link) {color: #fff !important;}`
   $.ajax({
-      url: 'url_to_css_file',
+      url: 'https://raw.githubusercontent.com/sbrych/slack-dark/master/black.css',
       success: function(css) {
           $("<style></style>").appendTo('head').html(css + tt__customCss);
       }
